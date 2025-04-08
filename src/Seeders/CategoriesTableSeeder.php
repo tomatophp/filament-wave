0df56c0444de0a3d8aa309b8c4f5ce7eea7e2997
Fady Mondy <EngFadyMondy@gmail.com>
2025-04-08 09:03:56 +0200

    fix seeder

--- src/Seeders/CategoriesTableSeeder.php
+++ src/Seeders/CategoriesTableSeeder.php
@@ -14,7 +14,7 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
 
-        \DB::table('categories')->truncate();
+        \DB::table('categories')->delete();
 
         \DB::table('categories')->insert([
             0 => [
